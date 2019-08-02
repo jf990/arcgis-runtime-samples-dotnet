137e5c49b02ab8718c25a227a3de306cb99e255c
Zack Allen <zack.m.allen@gmail.com>
2019-08-02 16:35:34 -0700

    Cleanup

--- src/Forms/Shared/Samples/Symbology/CustomDictionaryStyle/CustomDictionaryStyle.xaml.cs
+++ src/Forms/Shared/Samples/Symbology/CustomDictionaryStyle/CustomDictionaryStyle.xaml.cs
@@ -3,24 +3,18 @@
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific 
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
 // language governing permissions and limitations under the License.
 
+using ArcGISRuntime.Samples.Managers;
 using Esri.ArcGISRuntime.Data;
-using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
-using Esri.ArcGISRuntime.Tasks;
-using Esri.ArcGISRuntime.Tasks.Offline;
-using Esri.ArcGISRuntime.UI;
-using Esri.ArcGISRuntime.ArcGISServices;
-using Esri.ArcGISRuntime.UI.Controls;
-using Xamarin.Forms;
-using System.Collections.Generic;
 using System;
-using ArcGISRuntime.Samples.Managers;
+using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Forms;
 
 namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
 {
@@ -41,7 +35,7 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
         private string _currentStyle;
         private List<string> symbolFields;
 
-    public CustomDictionaryStyle()
+        public CustomDictionaryStyle()
         {
             InitializeComponent();
             Initialize();
@@ -93,7 +87,6 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
                 // Show the fields in the combo boxes.
                 //FoodPicker.ItemsSource = symbolFields;
 
-
                 // Set the map's initial extent to that of the restaurants.
                 map.InitialViewpoint = new Viewpoint(restaurantLayer.FullExtent);
 
@@ -105,44 +98,6 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
                 Console.WriteLine("**Exception: " + ex.ToString());
             }
         }
-
-        private static string GetStyleFilePath()
-        {
-            return DataManager.GetDataFolder("751138a2e0844e06853522d54103222a", "Restaurant.stylx");
-        }
-
-        private void Edit_Style_Clicked(object sender, System.EventArgs e)
-        {
-            // Disable button UI
-            MyMapView.IsVisible = false;
-            ButtonGrid.IsVisible = false;
-            // Enable Listview for styles
-            StylePicker.ItemsSource = _styleItems;
-            StylePickerUI.IsVisible = true;
-            PickerText.Text = "Select a style to edit.";
-        }
-
-        private void StylePicker_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            // Go to selected category
-            if(pickingCategory)
-            {
-                _currentStyle = ((KeyValuePair<string, string>)e.SelectedItem).Key;
-                StylePicker.ItemsSource = symbolFields;
-                pickingCategory = false;
-                PickerText.Text = "Select a field for "+_currentStyle;
-            }
-            else
-            {
-                // Set item based on category selection
-                _styleItems[_currentStyle] = e.SelectedItem as string ?? "";
-                pickingCategory = true;
-                MyMapView.IsVisible = true;
-                ButtonGrid.IsVisible = true;
-                StylePickerUI.IsVisible = false;
-            }
-        }
-
         private void Apply_Clicked(object sender, EventArgs e)
         {
             // Create overrides for expected field names that are different in this dataset.
@@ -176,16 +131,14 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
                 };
 
             // Set the text visibility configuration setting.
-            //add checkbox
-            //_restaurantStyle.Configurations.ToList().Find(c => c.Name == "text").Value = ShowTextCheckbox.IsChecked == true ? "ON" : "OFF";
             _restaurantStyle.Configurations.ToList().Find(c => c.Name == "text").Value = NameSwitch.IsToggled ? "ON" : "OFF";
 
             // Create the dictionary renderer with the style file and the field overrides.
-            DictionaryRenderer dictRenderr = new DictionaryRenderer(_restaurantStyle, styleToFieldMappingOverrides, textFieldOverrides);
+            DictionaryRenderer dictRenderer = new DictionaryRenderer(_restaurantStyle, styleToFieldMappingOverrides, textFieldOverrides);
 
             // Apply the dictionary renderer to the layer.
             var restaurantLayer = MyMapView.Map.OperationalLayers.First() as FeatureLayer;
-            restaurantLayer.Renderer = dictRenderr;
+            restaurantLayer.Renderer = dictRenderer;
         }
 
         private void ApplySimpleRendererClicked(object sender, EventArgs e)
@@ -195,6 +148,43 @@ namespace ArcGISRuntimeXamarin.Samples.CustomDictionaryStyle
             SimpleRenderer rendrr = new SimpleRenderer(markerSymbol);
             var restaurantLayer = MyMapView.Map.OperationalLayers.First() as FeatureLayer;
             restaurantLayer.Renderer = rendrr;
+        }
+
+        private static string GetStyleFilePath()
+        {
+            return DataManager.GetDataFolder("751138a2e0844e06853522d54103222a", "Restaurant.stylx");
+        }
+
+        private void Edit_Style_Clicked(object sender, System.EventArgs e)
+        {
+            // Disable button UI
+            MyMapView.IsVisible = false;
+            ButtonGrid.IsVisible = false;
+            // Enable Listview for styles
+            StylePicker.ItemsSource = _styleItems;
+            StylePickerUI.IsVisible = true;
+            PickerText.Text = "Select a style to edit.";
+        }
+
+        private void StylePicker_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // Go to selected category
+            if (pickingCategory)
+            {
+                _currentStyle = ((KeyValuePair<string, string>)e.SelectedItem).Key;
+                StylePicker.ItemsSource = symbolFields;
+                pickingCategory = false;
+                PickerText.Text = "Select a field for " + _currentStyle;
+            }
+            else
+            {
+                // Set item based on category selection
+                _styleItems[_currentStyle] = e.SelectedItem as string ?? "";
+                pickingCategory = true;
+                MyMapView.IsVisible = true;
+                ButtonGrid.IsVisible = true;
+                StylePickerUI.IsVisible = false;
+            }
         }
     }
     internal class ListTemplateSelector : DataTemplateSelector
